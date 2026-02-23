363290ecc0e7b8a479d5124e848b86906cc51c50
LorkaCP <brugnetti.attilio@gmail.com>
2026-02-24 00:37:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let allPlayers = []; // Stockage pour la recherche de joueurs
     
     // --- 1. CONFIGURATION & URLS ---
-    const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjnFfFWUPpHaWofmJ6UUEfw9VzAaaqTnS2WGm4pDSZxfs7FfEOOEfMprH60QrnWgROdrZU-s5VI9rR/pub?output=csv';
+    const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjnFfFWUPpHaWofmJ6UUEfw9VzAaaqTnS2WGm4pDSZxfs7FfEOOEfMprH60QrnWgROdrZU-s5VI9rR/pub?gid=252630071&single=true&output=csv';
     const PLAYERS_SHEET_BASE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjnFfFWUPpHaWofmJ6UUEfw9VzAaaqTnS2WGm4pDSZxfs7FfEOOEfMprH60QrnWgROdrZU-s5VI9rR/pub?single=true&output=csv&gid=';
     const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwxSJjTwYbTblXzrWbZ60n3PJc6LFeXFwJaz_Y-PTQrLcYRzJDkJbtR7gyvbTaGwdby4Q/exec'; 
     const CLIENT_ID = '1473807551329079408'; 
@@ -237,7 +237,7 @@ async function loadTeamsList() {
                     method: 'POST',
                     body: formData,
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    
+                    mode: 'no-cors'
                 });
                 alert("Profile update sent! Check back in a few moments.");
             } catch (error) {
@@ -774,11 +774,6 @@ document.getElementById('season-selector')?.addEventListener('change', (e) => {
     }
 
 }); // FIN DU DOMContentLoaded
-
-
-
-
-
 
 
 
